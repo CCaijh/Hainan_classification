9ef50cbb1c2b6a8a078ead77d3e242ad260cb6a1
CCaijh <128196565+CCaijh@users.noreply.github.com>
2025-07-15 17:36:23 +0800

    Update configs_0_5t.py

--- configs_0_5t.py
+++ configs_0_5t.py
@@ -6,14 +6,12 @@ train_configs={
       "lr":3e-4,
      "max_lr":0.003,
      "momentum":0.9,
-     "weights":"",  # 预训练权重路径，如果不想载入就设置为空字符
+     "weights":"",  # The pre-trained weight path should be set to an empty character if you do not want to load it
      "weight_decay":0.0001,
      "pct_start":0.1,
      "freeze_layers":False,
      "device":'cuda:0',
-     # "input_path":'Y:/ZMY/Hainan/Hainan_classification/modified_img/model_imgs',
-     # 5type
-     "input_path": 'Z:\ZQY\Hainan\Hainan_classification\HA_80_20_uniform',
+     "input_path": './HA419-IonogramSet-202505/HA_80_20_uniform',
      "output_path":'./logs',
 
      "seed": 2026,          # 固定随机种子
